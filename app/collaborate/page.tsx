5f7d25479341ca8bce759834cad7713920e13e22
Anton <nokker@gmail.com>
2023-06-29 14:50:02 +0900

    add collaborate google form

--- app/collaborate/page.tsx
+++ app/collaborate/page.tsx
@@ -1,7 +1,17 @@
 export default function Page() {
   return (
-    <section>
-      <h2>Collaborate</h2>
+    <section className=" flex justify-center">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScyBY1qxssbpxgC9AC-g4qXHzsm313zG42cI3LeGX8xC79q_Q/viewform?embedded=true"
+        width="640"
+        height={1600}
+        scrolling="no"
+        frameBorder="0"
+        marginHeight={0}
+        marginWidth={0}
+      >
+        Loading…
+      </iframe>
     </section>
   );
 }
