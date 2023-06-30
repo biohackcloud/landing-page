0212911bc9951024dae7fa8a16fbb19da26491e9
Anton <nokker@gmail.com>
2023-06-30 11:46:09 +0900

    remove unused parts

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 import fs from "fs/promises";
 import path from "path";
 import matter from "gray-matter";
-
 import { components, config } from "@/config/config.markdoc";
 import Markdoc from "@markdoc/markdoc";
 import React from "react";
@@ -15,21 +14,11 @@ async function getData() {
   const ast = Markdoc.parse(fileContents);
   const content = Markdoc.transform(ast, config);
   return { content };
-
-  // const contentHtml = processedContent.toString();
-
-  // return {
-  //   contentHtml,
-  // };
 }
 export default async function Page() {
   const { content } = await getData();
 
   return (
-    // <section
-    //   className=" markdown max-w-[600px] mx-auto my-10]"
-    //   dangerouslySetInnerHTML={{ __html: data.contentHtml }}
-    // ></section>
     <section className=" markdown max-w-[600px] mx-auto my-10]">
       {Markdoc.renderers.react(content, React, { components })}
     </section>
--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-const withMarkdoc = require("@markdoc/next.js");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  //   basePath: "/" + process.env.PAGES_BASE_URL || "",
-  //   assetPrefix: process.env.PAGES_BASE_URL
-  //     ? `/${process.env.PAGES_BASE_URL}/`
-  //     : "/",
 };
 
 module.exports = nextConfig;
