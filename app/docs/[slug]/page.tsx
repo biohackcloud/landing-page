f616c9e46756662381869e8f8f9f87d5ef6869fc
Anton Zhuravlev <55772081+neznayer@users.noreply.github.com>
2023-06-30 17:36:19 +0900

    Feature/docs (#16)
    * add docs
    
    * add sorting & categories
    
    * fix category font
    
    * make responsive
    
    * change tag of a category to h3
    
    * fix generate static params

--- app/docs/[slug]/page.tsx
+++ app/docs/[slug]/page.tsx
@@ -2,6 +2,24 @@ import { components } from "@/config/config.markdoc";
 import Markdoc from "@markdoc/markdoc";
 import React from "react";
 import { getMdData } from "@/utils/loadMdFile";
+import { readdir } from "fs/promises";
+import path from "path";
+
+export async function generateStaticParams() {
+  const docsDir = path.join(process.cwd(), "content", "docs");
+  const docs = await readdir(docsDir);
+
+  const slugs = await Promise.all(
+    docs.map(async (doc) => {
+      const docContent = await getMdData(["docs", doc]);
+      return docContent.slug;
+    })
+  );
+
+  return slugs.map((slug) => ({
+    slug,
+  }));
+}
 
 export default async function Page({ params }: { params: { slug: string } }) {
   const { content } = await getMdData(["docs", `${params.slug}.md`]);
